150c2425a1ff051defd0a9fa19695e252b15cb33
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2018-02-06 18:03:12 +0100

    Refactor handling of command line args

--- src/Main.hs
+++ src/Main.hs
@@ -2,25 +2,31 @@ module Main where
 
 import System.Environment
 
+import Data.Maybe
+
 import Voogie.Boogie.Parse
 import Voogie.Boogie.Pretty()
 import Voogie.Front
 import Voogie.Back
 import Voogie.TPTPretty
 
-parseArgs :: [String] -> (String, IO String)
-parseArgs []    = ("<stdin>", getContents)
-parseArgs (f:_) = (f        , readFile f)
+data Input = Input
+  { fileName :: Maybe String
+  , options :: TranslationOptions
+  }
+
+defaultOptions :: TranslationOptions
+defaultOptions = TranslationOptions True
+
+parseArgs :: [String] -> Input
+parseArgs args = Input (listToMaybe args) defaultOptions
 
 main :: IO ()
 main = do
   args <- getArgs
-  let (source, input) = parseArgs args
-  stream <- input
-  case parseAST source stream of
-    Left parsingError -> print parsingError
-    Right ast -> case analyze ast of
-      Left typeError -> putStrLn typeError
-      Right code -> putStr (prettyTPTP fool)
-        where opts = TranslationOptions True
-              fool = translate opts code
+  let input = parseArgs args
+  let source = fromMaybe "<stdin>" (fileName input)
+  stream <- maybe getContents readFile (fileName input)
+  let runTranslation = putStr . prettyTPTP . translate (options input)
+  let analyzeInput = either putStrLn runTranslation . analyze
+  either print analyzeInput (parseAST source stream)
