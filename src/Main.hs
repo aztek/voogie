35d4e3ae81eb9f6588ed1d7d028720a8fdfc8567
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2018-02-16 13:24:42 +0100

    Support the --action CL argument

--- src/Main.hs
+++ src/Main.hs
@@ -11,20 +11,36 @@ import Voogie.Front
 import Voogie.Back
 import Voogie.TPTPretty
 
+data Action = Parse | Check | Translate
+  deriving (Eq, Show, Enum, Bounded)
+
+actions = [("parse", Parse), ("check", Check), ("translate", Translate)]
+
+c :: String -> Maybe Action
+c s = lookup s actions
+
 data CmdArgs = CmdArgs
   { fileName :: Maybe String
+  , action :: Action
   , noArrayTheory :: Bool
   }
 
 cmdArgsParser :: Parser CmdArgs
 cmdArgsParser = CmdArgs
   <$> (fileName <|> stdIn)
+  <*> action
   <*> noArrayTheory
   where
     fileName = Just <$> strArgument (metavar "FILE")
     stdIn = flag' Nothing
       $ long "stdin"
      <> help "Read from the standart input rather than a file"
+    action = option (maybeReader c)
+      $ long "action"
+     <> metavar "ACTION"
+     <> value Translate
+     <> help ("Action to perform, can be one of the following: " ++
+              "parse, check, translate (default)")
     noArrayTheory = switch
       $ long "no_array_theory"
      <> help "Do not use polymorhic theory of arrays"
@@ -32,7 +48,7 @@ cmdArgsParser = CmdArgs
 cmdArgsParserInfo :: ParserInfo CmdArgs
 cmdArgsParserInfo = info (cmdArgsParser <**> helper)
   $ fullDesc
- <> header ("voogie - a verification conditions generator " ++
+ <> header ("Voogie - a verification conditions generator " ++
             "for simple Boogie programs")
 
 collectOptions :: CmdArgs -> TranslationOptions
@@ -41,8 +57,15 @@ collectOptions cmdArgs = TranslationOptions (not $ noArrayTheory cmdArgs)
 main :: IO ()
 main = do
   cmdArgs <- execParser cmdArgsParserInfo
+  let options = collectOptions cmdArgs
   let source = fromMaybe "<stdin>" (fileName cmdArgs)
   stream <- maybe getContents readFile (fileName cmdArgs)
-  let runTranslation = putStr . prettyTPTP . translate (collectOptions cmdArgs)
-  let analyzeInput = either putStrLn runTranslation . analyze
-  either print analyzeInput (parseAST source stream)
+
+  let runParser f = either print f (parseAST source stream)
+  let runAnalyzer f = either putStrLn f . analyze
+  let runTranslator = translate options
+
+  case Main.action cmdArgs of
+    Parse -> runParser print
+    Check -> runParser . runAnalyzer $ (putStr . pretty)
+    Translate -> runParser . runAnalyzer $ (putStr . prettyTPTP . runTranslator)
