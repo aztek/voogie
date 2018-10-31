93069f5fa628827512cf0ed572e77454f7122af3
aztek <evgeny.kotelnikov@gmail.com>
2018-10-31 02:43:50 +0100

    Fix compilation for GHC 7.10

--- src/Voogie/Boogie/Parse.hs
+++ src/Voogie/Boogie/Parse.hs
@@ -1,6 +1,6 @@
 module Voogie.Boogie.Parse (parseAST) where
 
-import Control.Monad (guard, sequence_)
+import Control.Monad --(guard, sequence_)
 import Data.Maybe
 import qualified Data.List.NonEmpty as NE
 
