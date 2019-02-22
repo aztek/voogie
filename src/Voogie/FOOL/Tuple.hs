dfb9f525fd5c9ea40390e02549d3ac78d700512c
aztek <evgeny.kotelnikov@gmail.com>
2019-02-22 17:36:23 +0100

    Use NE.nonEmpty

--- src/Voogie/FOOL/Tuple.hs
+++ src/Voogie/FOOL/Tuple.hs
@@ -14,8 +14,9 @@ toNonEmpty :: Tuple a -> NonEmpty a
 toNonEmpty (a :| ne) = NE.cons a ne
 
 nonUnit :: NonEmpty a -> Either a (Tuple a)
-nonUnit (a NE.:| [])  = Left a
-nonUnit (a NE.:| b:c) = Right (a :| (b NE.:| c))
+nonUnit (a NE.:| as)
+  | Just as' <- NE.nonEmpty as = Right (a :| as')
+  | otherwise = Left a
 
 intercalate :: [a] -> Tuple [a] -> [a]
 intercalate l = L.intercalate l . toList
