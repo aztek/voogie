f16acefbce89f3ca9e5473675170422323fd758c
aztek <evgeny.kotelnikov@gmail.com>
2019-05-29 15:27:37 +0200

    Refactor Voogie.FOOL.Rewrite

--- src/Voogie/FOOL/Rewrite.hs
+++ src/Voogie/FOOL/Rewrite.hs
@@ -15,13 +15,15 @@ type Rewriter a t = t -> Maybe (Accumulate a t)
 rewriteType :: Monoid m => Rewriter m Type -> Type -> Accumulate m Type
 rewriteType typec t
   | Just c <- typec t = c
-  | otherwise = case t of
-    Array ts t -> Array <$> traverse inline ts <*> inline t
-    TupleType ts -> TupleType <$> traverse inline ts
-    Functional ts t -> Functional <$> traverse inline ts <*> inline t
-    _ -> pure t
+  | otherwise = inlineType t
   where
     inline = rewriteType typec
+
+    inlineType = \case
+      Array      ts t -> Array      <$> traverse inline ts <*> inline t
+      TupleType    ts -> TupleType  <$> traverse inline ts
+      Functional ts t -> Functional <$> traverse inline ts <*> inline t
+      t               -> pure t
 
 rewriteSymbol :: Monoid m => Rewriter m Type -> Typed a -> Accumulate m (Typed a)
 rewriteSymbol typec i@(Typed t a)
@@ -32,29 +34,28 @@ rewriteTerm :: Monoid m =>
                Rewriter m Term -> Rewriter m Type -> Term -> Accumulate m Term
 rewriteTerm termc typec t
   | Just c <- termc t = c
-  | otherwise = case t of
-    IntegerConstant{} -> pure t
-    BooleanConstant{} -> pure t
-    Variable v -> Variable <$> rewriteSymbol typec v
-    Constant i -> Constant <$> rewriteSymbol typec i
-    Application f ts -> Application <$> rewriteSymbol typec f <*> traverse inline ts
-    Binary op f g -> Binary op <$> inline f <*> inline g
-    Unary op f -> Unary op <$> inline f
-    Quantify q vs f -> Quantify q <$> traverse (rewriteSymbol typec) vs <*> inline f
-    Equals s a b -> Equals s <$> inline a <*> inline b
-    Let (Binding d b) t -> Let <$> b' <*> inline t
-      where
-        b' = Binding <$> inlineDefinition d <*> inline b
-    If c a b -> If <$> inline c <*> inline a <*> inline b
-    Select a i -> Select <$> inline a <*> inline i
-    Store a i v -> Store <$> inline a <*> inline i <*> inline v
-    TupleLiteral es -> TupleLiteral <$> traverse inline es
+  | otherwise = inlineTerm t
   where
     inline = rewriteTerm termc typec
 
-    inlineDefinition (ConstantSymbol i) =
-      ConstantSymbol <$> rewriteSymbol typec i
-    inlineDefinition (Function i vs) =
-      Function <$> rewriteSymbol typec i <*> traverse (rewriteSymbol typec) vs
-    inlineDefinition (TupleD is) =
-      TupleD <$> traverse (rewriteSymbol typec) is
+    inlineTerm = \case
+      Variable        v -> Variable     <$> rewriteSymbol typec v
+      Constant        i -> Constant     <$> rewriteSymbol typec i
+      Application  f ts -> Application  <$> rewriteSymbol typec f <*> traverse inline ts
+      Binary     op f g -> Binary op    <$> inline f <*> inline g
+      Unary        op f -> Unary op     <$> inline f
+      Quantify   q vs f -> Quantify q   <$> traverse (rewriteSymbol typec) vs <*> inline f
+      Equals      s a b -> Equals s     <$> inline a <*> inline b
+      Let           b t -> Let          <$> inlineBinding b <*> inline t
+      If          c a b -> If           <$> inline c <*> inline a <*> inline b
+      Select        a i -> Select       <$> inline a <*> inline i
+      Store       a i v -> Store        <$> inline a <*> inline i <*> inline v
+      TupleLiteral   es -> TupleLiteral <$> traverse inline es
+      t                 -> pure t
+
+    inlineBinding (Binding d b) = Binding <$> inlineDefinition d <*> inline b
+
+    inlineDefinition = \case
+      ConstantSymbol i -> ConstantSymbol <$> rewriteSymbol typec i
+      Function    i vs -> Function       <$> rewriteSymbol typec i <*> traverse (rewriteSymbol typec) vs
+      TupleD        is -> TupleD         <$> traverse (rewriteSymbol typec) is
