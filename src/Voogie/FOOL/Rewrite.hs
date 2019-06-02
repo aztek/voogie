30dca4be529b1dfeffc281da91d6f8466902bedc
aztek <evgeny.kotelnikov@gmail.com>
2019-06-02 13:22:04 +0200

    Slightly better names

--- src/Voogie/FOOL/Rewrite.hs
+++ src/Voogie/FOOL/Rewrite.hs
@@ -33,12 +33,12 @@ traverseTerm :: Applicative f
              -> (Type -> f Type)
              -> Term -> f Term
 traverseTerm termF typeF = \case
-  Variable       v -> Variable     <$> traverseTyped typeF v
-  Constant       i -> Constant     <$> traverseTyped typeF i
-  Application f ts -> Application  <$> traverseTyped typeF f <*> traverse termF ts
+  Variable       v -> Variable     <$> typedF v
+  Constant       i -> Constant     <$> typedF i
+  Application f ts -> Application  <$> typedF f <*> traverse termF ts
   Binary    op f g -> Binary op    <$> termF f <*> termF g
   Unary       op f -> Unary op     <$> termF f
-  Quantify  q vs f -> Quantify q   <$> traverse (traverseTyped typeF) vs <*> termF f
+  Quantify  q vs f -> Quantify q   <$> traverse typedF vs <*> termF f
   Equals     s a b -> Equals s     <$> termF a <*> termF b
   Let          b t -> Let          <$> traverseBinding termF typeF b <*> termF t
   If         c a b -> If           <$> termF c <*> termF a <*> termF b
@@ -46,6 +46,8 @@ traverseTerm termF typeF = \case
   Store      a i v -> Store        <$> termF a <*> termF i <*> termF v
   TupleLiteral  es -> TupleLiteral <$> traverse termF es
   t                -> pure t
+  where
+    typedF = traverseTyped typeF
 
 traverseBinding :: Applicative f
                 => (Term -> f Term)
@@ -58,8 +60,8 @@ traverseDefinition :: Applicative f
                    => (Type -> f Type)
                    -> Definition -> f Definition
 traverseDefinition typeF = \case
-  ConstantSymbol i -> ConstantSymbol <$> traverseSymbol i
-  Function    i vs -> Function       <$> traverseSymbol i <*> traverse traverseSymbol vs
-  TupleD        is -> TupleD         <$> traverse traverseSymbol is
+  ConstantSymbol i -> ConstantSymbol <$> typedF i
+  Function    i vs -> Function       <$> typedF i <*> traverse typedF vs
+  TupleD        is -> TupleD         <$> traverse typedF is
   where
-    traverseSymbol = traverseTyped typeF
+    typedF = traverseTyped typeF
