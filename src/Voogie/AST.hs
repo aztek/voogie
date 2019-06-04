be50403456f1a7d5546242d5aadcffd0d1e578a9
aztek <evgeny.kotelnikov@gmail.com>
2019-06-04 12:43:36 +0200

    Explicitly import Foldable and Traversable with older base

--- src/Voogie/AST.hs
+++ src/Voogie/AST.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE CPP #-}
 
 module Voogie.AST where
+
+#if !MIN_VERSION_base(4, 8, 0)
+import Data.Foldable (Foldable)
+import Data.Traversable (Traversable)
+#endif
 
 import Text.Parsec.Pos (SourcePos)
 
