a25ba3ffe9748b099284a6a8c8711292a7655cfe
aztek <evgeny.kotelnikov@gmail.com>
2019-06-04 13:10:19 +0200

    Import Data.Monoid for older base

--- src/Voogie/FOOL.hs
+++ src/Voogie/FOOL.hs
@@ -19,6 +19,11 @@ module Voogie.FOOL (
 
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonUnit (NonUnit)
+
+#if !MIN_VERSION_base(4, 8, 0)
+import Data.Monoid (Monoid(..))
+#endif
+
 #if !MIN_VERSION_base(4, 11, 0)
 import Data.Semigroup (Semigroup(..))
 #endif
