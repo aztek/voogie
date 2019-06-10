fa729db62ca7def768bafb8b1ebbbaa4f1e461f7
aztek <evgeny.kotelnikov@gmail.com>
2019-06-10 10:39:19 +0200

    Remove the unused intercalate function

--- src/Data/List/NonEmpty.hs
+++ src/Data/List/NonEmpty.hs
@@ -5,8 +5,6 @@ module Data.List.NonEmpty (
   module Data.List.NonEmpty
 ) where
 
-import qualified Data.List as L (intercalate)
-
 #if MIN_VERSION_base(4, 9, 0)
 import "base" Data.List.NonEmpty
 import qualified "base" Data.List.NonEmpty as NE (zipWith)
@@ -14,9 +12,6 @@ import qualified "base" Data.List.NonEmpty as NE (zipWith)
 import "semigroups" Data.List.NonEmpty
 import qualified "semigroups" Data.List.NonEmpty as NE (zipWith)
 #endif
-
-intercalate :: [a] -> NonEmpty [a] -> [a]
-intercalate l = L.intercalate l . toList
 
 zipWithM :: Applicative m
          => (a -> b -> m c) -> NonEmpty a -> NonEmpty b -> m (NonEmpty c)
--- src/Data/List/NonUnit.hs
+++ src/Data/List/NonUnit.hs
@@ -2,7 +2,6 @@
 
 module Data.List.NonUnit where
 
-import qualified Data.List as L (intercalate)
 import qualified Data.List.NonEmpty as NE (toList, cons, nonEmpty, NonEmpty(..))
 import Data.List.NonEmpty (NonEmpty)
 
@@ -19,6 +18,3 @@ nonUnit :: NonEmpty a -> Either a (NonUnit a)
 nonUnit (a NE.:| as)
   | Just as' <- NE.nonEmpty as = Right (a :| as')
   | otherwise = Left a
-
-intercalate :: [a] -> NonUnit [a] -> [a]
-intercalate l = L.intercalate l . toList
